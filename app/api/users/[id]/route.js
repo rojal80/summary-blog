3fe412e0ff13aec1a275b7dc9e2d14f65a5b0ccb
Rojal Kadariya <rojalkadariya198@gmail.com>
2023-10-31 16:31:36 +0545

    email

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -5,38 +5,29 @@ import User from "@/models/UserModel";
 // @desc    Get user based on the user id
 // @route   GET /api/users/:id
 export async function GET(request, { params }) {
-  try {
-    await connectDB();
-    const { id } = params;
-
-    const { searchParams } = new URL(request.url);
-    const type = searchParams.get("type");
-
-    let blogs;
-    if (type === "id") {
+   try {
+      await connectDB();
+      const { id } = params;
       const user = await User.findById(id);
       return NextResponse.json(user, { status: 200 });
-    } else if (type === "email") {
-      const user = await User.find({ email: id });
-    }
-  } catch (err) {
-    console.log(err.message);
-  }
+   } catch (err) {
+      console.log(err.message);
+   }
 }
 
 // @desc   update user details based on the user id
 // @route  PUT /api/users/:id
 export async function PUT(request, { params }) {
-  try {
-    await connectDB();
-    const { id } = params;
-    const data = await request.json();
-    const response = await User.findByIdAndUpdate(id, data, {
-      new: true,
-      runValidators: true,
-    });
-    return NextResponse.json(response, { status: 200 });
-  } catch (err) {
-    console.log(err.message);
-  }
+   try {
+      await connectDB();
+      const { id } = params;
+      const data = await request.json();
+      const response = await User.findByIdAndUpdate(id, data, {
+         new: true,
+         runValidators: true,
+      });
+      return NextResponse.json(response, { status: 200 });
+   } catch (err) {
+      console.log(err.message);
+   }
 }
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -4,25 +4,35 @@ import User from "@/models/UserModel";
 
 // @desc    Get all users
 // @route   GET /api/users
-export async function GET() {
-   try {
-      const users = await User.find();
-      return NextResponse.json(users, { status: 200 });
-   } catch (err) {
-      console.log(err.message);
-   }
+export async function GET(request, { params }) {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(request.url);
+    const email = searchParams.get("email");
+
+    if (email) {
+      const user = await User.find({ email });
+      return NextResponse.json(user, { status: 200 });
+    } else {
+      const user = await User.find();
+      return NextResponse.json(user, { status: 200 });
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 // @desc    Create a blog
 // @route   POST /api/users
 export async function POST(request) {
-   try {
-      await connectDB();
-      const req = await request.json();
-      const res = await User.create(req);
-      return NextResponse.json(res, { status: 201 });
-   } catch (err) {
-      // error(err);
-      console.log(err.message);
-   }
+  try {
+    await connectDB();
+    const req = await request.json();
+    const res = await User.create(req);
+    return NextResponse.json(res, { status: 201 });
+  } catch (err) {
+    // error(err);
+    console.log(err.message);
+  }
 }
