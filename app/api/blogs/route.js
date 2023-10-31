c23950d9ee65e01cee8b1b9dd41e0ccdd45ccc4b
Rojal Kadariya <rojalkadariya198@gmail.com>
2023-10-31 14:07:56 +0545

    accepted post issue solved

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -5,34 +5,36 @@ import Blog from "@/models/BlogModel";
 // @desc    Get all blogs
 // @route   GET /api/blogs
 export async function GET(request, { params }) {
-   try {
-      await connectDB();
+  try {
+    await connectDB();
 
-      const { searchParams } = new URL(request.url);
-      const status = searchParams.get("status");
+    const { searchParams } = new URL(request.url);
+    const status = searchParams.get("status");
 
-      let blogs;
-      if (status === "pending") {
-         blogs = await Blog.find({ status: "pending" });
-      } else {
-         blogs = await Blog.find();
-      }
-      return NextResponse.json(blogs, { status: 200 });
-   } catch (err) {
-      console.log(err.message);
-   }
+    let blogs;
+    if (status === "pending") {
+      blogs = await Blog.find({ status: "pending" });
+    } else if (status === "accepted") {
+      blogs = await Blog.find({ status: "accepted" });
+    } else {
+      blogs = await Blog.find();
+    }
+    return NextResponse.json(blogs, { status: 200 });
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 // @desc    Create a blog
 // @route   POST /api/blogs
 export async function POST(request) {
-   try {
-      await connectDB();
-      const req = await request.json();
-      const res = await Blog.create(req);
-      return NextResponse.json(res, { status: 201 });
-   } catch (err) {
-      // error(err);
-      console.log(err.message);
-   }
+  try {
+    await connectDB();
+    const req = await request.json();
+    const res = await Blog.create(req);
+    return NextResponse.json(res, { status: 201 });
+  } catch (err) {
+    // error(err);
+    console.log(err.message);
+  }
 }
