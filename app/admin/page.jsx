b12d5a916d2ff43219b30c9c38ff6e4ef66c5620
Rojal Kadariya <rojalkadariya198@gmail.com>
2023-11-05 11:38:04 +0545

    card

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -3,23 +3,23 @@ import Card from "@/components/shared/Card";
 import { useContext } from "react";
 import BlogContext from "@/components/context/BlogContext";
 
-import CardLoading from "@/components/loading/cardLoading";
+import CardLoading from "@/components/loading/CardLoading";
 
 import { Suspense } from "react";
 
 export default function HomePage() {
-   const { pendingPosts, posts, users } = useContext(BlogContext);
-   return (
-      <div className=" w-fit mx-5 pt-5 flex flex-wrap gap-5">
-         <Suspense fallback={CardLoading}>
-            <Card title="pending posts" number={pendingPosts.length} />
-         </Suspense>
-         <Suspense fallback={CardLoading}>
-            <Card title="accepted posts" number={posts.length} />
-         </Suspense>
-         <Suspense fallback={CardLoading}>
-            <Card title="users" number={users.length} />
-         </Suspense>
-      </div>
-   );
+  const { pendingPosts, posts, users } = useContext(BlogContext);
+  return (
+    <div className=" w-fit mx-5 pt-5 flex flex-wrap gap-5">
+      <Suspense fallback={CardLoading}>
+        <Card title="pending posts" number={pendingPosts.length} />
+      </Suspense>
+      <Suspense fallback={CardLoading}>
+        <Card title="accepted posts" number={posts.length} />
+      </Suspense>
+      <Suspense fallback={CardLoading}>
+        <Card title="users" number={users.length} />
+      </Suspense>
+    </div>
+  );
 }
